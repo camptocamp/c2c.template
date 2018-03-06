22d1e6ba3589377759e4c1de6ec0f202011b3fc8
Stéphane Brunner <stephane.brunner@camptocamp.com>
2018-03-06 16:41:53 +0100

    Do the release 2.0.8

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ install_requires = [
 
 setup(
     name='c2c.template',
-    version='2.0.7',
+    version='2.0.8',
     description='Vars collector and template runner.',
     long_description=README,
     namespace_packages=['c2c'],
